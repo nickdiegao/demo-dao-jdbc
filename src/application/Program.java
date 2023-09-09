6ec265cab5fb24c0c6e20be3cf8914e5b0143763
Nicholas Diego <nickdiegao@gmail.com>
2023-09-09 13:38:41 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,25 +1,17 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 	
 	public static void main(String[] args) {
+		SellerDao sellerdao = DaoFactory.createSellerDao();
 		
-		Department obj = new Department(1, "books");
-		
-		System.out.println(obj);
-		
-		Seller seller = new Seller(21,"Bob","bob@gmail.com",new Date(), 3000.0, obj);
+		Seller seller = sellerdao.findById(4);
 		
 		System.out.println(seller);
-		
-		SellerDao sellerdao = DaoFactory.createSellerDao();
 		
 	}
 }
